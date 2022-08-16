d21b5e071204a7de92d718f76e7b8e5457f59a06
Diana Murtazina <murka8831@yandex.ru>
2022-08-16 15:09:35 +0400

    app files added

---
+++ calculator/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+    return (
+        <React.Fragment>
+            <header>
+                <h1>Кошелек</h1>
+            </header>
+
+            <main>Главный блок</main>
+        </React.Fragment>
+    );
+}
+
+export default App;
